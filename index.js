002ef401bbe777309a686ceab950c8722b78a7e9
Richard Power <richardpwr271@gmail.com>
2025-09-30 15:09:43 -0400

    added the getplayer function

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 // === Constants ===
-const BASE = "https://fsa-puppy-bowl.herokuapp.com/api";
-const COHORT = "/"; // Make sure to change this!
-const API = BASE + COHORT;
+const BASE = "https://fsa-puppy-bowl.herokuapp.com/api/";
+const COHORT = "2508-FTB-ET-WEB-FT"; // Make sure to change this!
+const RESOURCE = "/players";
+const API = BASE + COHORT + RESOURCE;
+
+let players = [];
+let selectedPlayer;
+
+async function getPlayers() {
+  try {
+    const response = await fetch(API);
+    const result = await response.json();
+    players = result.data.players;
+    // console.log(players[0]);
+    render();
+  } catch (error) {
+    console.error("Uh oh, trouble fetching players!", err);
+  }
+}
